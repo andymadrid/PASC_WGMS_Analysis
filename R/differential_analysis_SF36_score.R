3872783e37f912297d6302655bf6c2667bc4b1ab
Andy Madrid <76069657+andymadrid@users.noreply.github.com>
2024-07-12 12:46:46 -0500

    Update differential_analysis_SF36_score.R

--- R/differential_analysis_SF36_score.R
+++ R/differential_analysis_SF36_score.R
@@ -4,7 +4,7 @@ targets.sub <- targets[which(targets$Group=="LC"),]
 pData(bs.sub)$SF.36_Score <- targets.sub$SF.36_Score
 dss.formula <- formula(~ SF.36_Score + Age + Sex + BMI + type1 + type2 + type3 + type4 + type5 + PC1 + PC2)
 design.df <- pData(bs.sub)
-dml.fit <- DMLfit.multiFactor(bs.sub, design = design.df, smoothing = TRUE,  smoothing.span = 500, formula = dss.formula)
+dml.fit <- DMLfit.multiFactor(bs.sub, design = design.df, smoothing = TRUE,  smoothing.span = 150, formula = dss.formula)
 test.result <- DMLtest.multiFactor(dml.fit, coef = 2)
 median(qchisq(1-test.result$pvals,1))/qchisq(0.5,1)
 # 0.4675222
@@ -13,4 +13,5 @@ ss <- fdrtool(test.result$stat, statistic="normal", plot=F)
 # 1.038744
 test.result$p.from.ss <- ss$pval
 test.result$lfdr <- ss$lfdr
-save(bs.sub, test.result, file = "Results/SF_Score/dssResults.SF36_Score.smooth500.rdata")
+save(bs.sub, test.result, file = "Results/SF_Score/dssResults.SF36_Score.smooth150.rdata")
+
